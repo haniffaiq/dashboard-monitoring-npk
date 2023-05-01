60c321712ce03b48f8d7d5f5c361f86e609ac89f
syahdan41 <syahdantriantoro41@gmail.com>
2023-05-01 23:37:30 +0700

    regist & login syahdan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login/Login";
+import Registrasi from "./pages/Registrasi/Registrasi";
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      {/* <Homepage /> */}
+      {/* <Login /> */}
+      <Registrasi />
     </div>
   );
 }
